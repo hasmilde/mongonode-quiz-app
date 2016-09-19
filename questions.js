559b9bac86e2bc30b01998c1f908543fb0afa99c
baltena <bart.altena@hotmail.com>
2016-09-19 21:13:22 +0200

    Update questions.js
    Added some pseudo code explaining what to build for getting and posting answers between client (Player) and server.

--- questions.js
+++ questions.js
@@ -7,6 +7,41 @@ var QuestionsSchema   = new Schema({
     category: String
 });
 
+//This function is handling the Response for the GET call from the Player of the quizz
+//It returns the questions with the answers but without the correctness of the answer, because otherwise this can be seen in the response in client.
+Questions.getByPlayer() = {
+    return {
+        questions: [
+            {
+              _id: a121d12e1,
+              questionBody: "tralalalala",
+              answers: [{_id:ae112, answerBody: "aaaa"}]
+            }
+            ]
+    }
+}
+
+// This function is handling the POST call when the Player submitted 1 answer.
+/* format of this post should be: 
+    { questionID: 21as231fs,
+      givenAnswers [{_id:ae112, answerValid: true},{_id:ae112, answerValid: true}]
+    }
+*/
+//It returns the givenAnswers and whether those were valid or not. 
+Questions.checkAnswers() = {
+    
+    //TO-DO: write function to query on questionID
+    //TO-DO: write function to loop though question by answerID
+    //TO-DO: write function to check answers against given answer
+    
+    //This is what the response should be to the POST call
+    return {
+        givenAnswers: [
+            {_id:ae112, answerValid: true}
+            ]
+    }
+}
+
 
 Team.find({  
     'GroupName': gname
